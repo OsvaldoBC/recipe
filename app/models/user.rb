2647b4cae281f370a4e05641917a4e223776577b
gdumani <gdumani@gmail.com>
2023-06-27 11:29:15 -0600

    Add validations to recipe

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many: foods
-  has_many: recipes
+  has_many foods
+  has_many recipes
 
-  validates: :name, presence:true, length: { maximum 50}
-
+  validates :name, presence: true, length: { maximum: 50}
 end
