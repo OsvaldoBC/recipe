59840ce991cc65dbb233d35268a48c6cc3aefc64
gdumani <gdumani@gmail.com>
2023-06-28 15:06:37 -0600

    add methods to recipes controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'recipes/index'
-  get 'recipes/show'
-  get 'recipes/create'
-  get 'recipes/delete'
+  get 'recipes', to: 'recipes#index', as: 'recipes'
+  get 'recipes_public', to: 'recipes#public', as: 'recipes_public'
+  get 'recipes/new', to: 'recipes#new', as: 'recipes_new'
+  post 'recipes/new', to: 'recipes#create', as: 'recipes_create'
+  get 'recipes/id', to: 'recipes#show', as: 'recipes_show'
+  delete 'recipes/id', to: 'recipes#destroy', as: 'delete_recipe'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
