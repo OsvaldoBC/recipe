9fda4a82a2449303896521cbb6e16d41291febe6
gdumani <gdumani@gmail.com>
2023-06-28 15:06:37 -0600

    add methods to recipes controller and routes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,39 @@
 class RecipesController < ApplicationController
   def index
+    if current_user.nil?
+      redirect_to new_user_session
+    else
+      @user = current_user
+      @recipes = @user.recipes
+    end
+  end
+
+  def public
+    @recipes = Recipe.filter_by(public)
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
   end
 
   def create
+    @recipe = Recipe.new user: current_user, name: params[:name], preparation_time: params[:preparation_time],
+                         cooking_time: params[:cooking_time], description: params[:description], public: false
+    if @recipe.save
+      redirect_to recipe_show_path(@recipe.id)
+    else
+      render 'new'
+    end
   end
 
-  def delete
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    flash[:success] = 'Recipe delete'
+    redirect_to recipes_path
   end
 end
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,22 +1,22 @@
-require "test_helper"
+require 'test_helper'
 
 class RecipesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get recipes_index_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get recipes_show_url
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get recipes_create_url
     assert_response :success
   end
 
-  test "should get delete" do
+  test 'should get delete' do
     get recipes_delete_url
     assert_response :success
   end
