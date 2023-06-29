bd5849947e70513f135f9f5340ac8498423409f9
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-06-29 14:06:33 -0500

    Add Navegation menu and style

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,24 @@ Rails.application.routes.draw do
   delete 'recipes/:id', to: 'recipes#destroy', as: 'delete_recipe'
   post 'recipe_food/new', to: 'recipe_food#create', as: 'recipe_food_create'
   delete 'recipe_food/:id', to: 'recipe_food#destroy', as: 'recipe_food_delete'
+  
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'recipes#index', as: 'root'
   get '/foods', to: 'foods#index'
   get '/foods/new', to: 'foods#new', as: 'new_food'
   post '/foods/new', to: 'foods#create', as: 'create_food'
   delete '/foods/:id', to: 'foods#destroy', as: 'delete_food'
+
+  get 'recipes', to: 'recipes#index', as: 'recipes'
+  get 'recipes_public', to: 'recipes#public', as: 'recipes_public'
+  get 'recipes/new', to: 'recipes#new', as: 'recipes_new'
+  post 'recipes/new', to: 'recipes#create', as: 'recipes_create'
+  get 'recipes/:id', to: 'recipes#show', as: 'recipes_show'
+  delete 'recipes/:id', to: 'recipes#destroy', as: 'delete_recipe'
+
+  get 'shopping_list', to: 'shopping_list#index', as: 'shopping_list'
 end
